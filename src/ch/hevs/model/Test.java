8a8c1748b4d3670322bbdc9a6887d3d2defad54b
maniac1989 <pierry_wyssen@hotmail.com>
2014-12-29 17:38:23 +0100

    added add + multiply method

--- src/ch/hevs/model/Test.java
+++ src/ch/hevs/model/Test.java
@@ -4,12 +4,18 @@ public class Test {
 
 	private int number1;
 	private int number2;
-	
-	public Test() {}
-	
+
 	public Test(int number1, int number2) {
 		this.number1 = number1;
 		this.number2 = number2;
+	}
+	
+	public int add() {
+		return number1 + number2;
+	}
+	
+	public int multiply() {
+		return number1 * number2;
 	}
 	
 	public int getNumber1() {
