6c4d9b8282b0dd6d3c999c9e92e7abdde2802266
maniac1989 <pierry_wyssen@hotmail.com>
2014-12-30 17:55:32 +0100

    added comments

--- src/ch/hevs/model/Maths.java
+++ src/ch/hevs/model/Maths.java
@@ -5,6 +5,9 @@ public class Maths {
 	private int number1;
 	private int number2;
 
+	/**
+	 * Constructor
+	 */
 	public Maths(int number1, int number2) {
 		this.number1 = number1;
 		this.number2 = number2;
