f59a586d83c7b9726c418c89459759262c8b738c
wyssenp <pierry_wyssen@hotmail.com>
2014-12-30 17:10:13 +0100

    added comments

--- src/ch/hevs/test/Testing.java
+++ src/ch/hevs/test/Testing.java
@@ -8,12 +8,18 @@ import ch.hevs.model.Maths;
 
 public class Testing {
 
+	/**
+	 * Test the add() method
+	 */
 	@Test
 	public void testAdding() {
 		Maths n1 = new Maths(3,5);
 		assertEquals(8, n1.add());
 	}
 	
+	/**
+	 * Test the multiply() method
+	 */
 	@Test
 	public void testMultiplying() {
 		Maths n2 = new Maths(5,5);
